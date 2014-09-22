96dd02441ac87d9ed5b484a0acf7e804cf54fd3a
Jonathan Potter <jpotterm@gmail.com>
2014-09-22 00:07:39 -0400

    Better help

--- Main.hs
+++ Main.hs
@@ -20,79 +20,94 @@ import Util
 
 data Command = Command { name :: String
                        , function :: [String] -> String -> IO ()
-                       , docArgs :: String
-                       , docDescription :: String
+                       , docSummary :: String
+                       , doc :: String
                        }
 
 commands :: [Command]
 commands = [ Command { name="init"
                      , function=initCommand
-                     , docArgs=""
-                     , docDescription="Initialize manila project in the current directory"
+                     , docSummary="Initialize manila project in the current directory"
+                     , doc="manila init\n\nInitialize manila project in the current directory."
                      }
            , Command { name="migrate"
                      , function=migrateCommand
-                     , docArgs=""
-                     , docDescription="Migrate the database to a newer version of manila"
+                     , docSummary="Migrate the database to a newer version of manila"
+                     , doc="manila migrate\n\nMigrate the database to a newer version of manila."
                      }
            , Command { name="help"
                      , function=helpCommand
-                     , docArgs=""
-                     , docDescription="Print this list"
+                     , docSummary="Print this list"
+                     , doc="manila help\n\nPrint this list."
                      }
            , Command { name="pull"
                      , function=pullCommand
-                     , docArgs=""
-                     , docDescription="Download and import transactions and account balances from Mint.com"
+                     , docSummary="Download and import transactions and account balances from Mint.com"
+                     , doc="manila pull\n\nDownload and import transactions and account balances from Mint.com."
                      }
            , Command { name="envelope"
                      , function=envelopeCommand
-                     , docArgs=""
-                     , docDescription="Add an envelope"
+                     , docSummary="Add an envelope"
+                     , doc="manila envelope <name>\n\nAdd an envelope."
                      }
            , Command { name="transfer"
                      , function=transferCommand
-                     , docArgs=""
-                     , docDescription="Transfer money to an envelope (use a negative amount to transfer from envelope)"
+                     , docSummary="Transfer money to an envelope (use a negative amount to transfer from envelope)"
+                     , doc="manila transfer <amount> <envelope>\n\nTransfer money to an envelope (use a negative amount to transfer from envelope)."
                      }
            , Command { name="summary"
                      , function=summaryCommand
-                     , docArgs=""
-                     , docDescription="Summary of envelope and account balances"
+                     , docSummary="Summary of envelope and account balances"
+                     , doc="manila summary\n\nSummary of envelope and account balances."
                      }
            , Command { name="attach"
                      , function=attachCommand
-                     , docArgs=""
-                     , docDescription="Attach Mint.com category to envelope"
+                     , docSummary="Attach Mint.com category to envelope"
+                     , doc="manila attach <envelope> <category>\n\nAttach Mint.com category to envelope."
                      }
            , Command { name="categories"
                      , function=categoriesCommand
-                     , docArgs=""
-                     , docDescription="List the categories that are attached to an envelope"
+                     , docSummary="List the categories that are attached to an envelope"
+                     , doc="manila categories\n\nList the categories that are attached to an envelope."
                      }
            , Command { name="rule"
                      , function=addRuleCommand
-                     , docArgs=""
-                     , docDescription="Add a rule"
+                     , docSummary="Add a rule"
+                     , doc="manila rule -c <envelope> <category> <percentage> <amount> [start_date]\nmanila rule -t <envelope> <frequence> <amount> [start_date]\n\nAdd a rule."
                      }
            , Command { name="rules"
                      , function=listRulesCommand
-                     , docArgs=""
-                     , docDescription="List all rules"
+                     , docSummary="List all rules"
+                     , doc="manila rules\n\nList all rules."
                      }
            ]
 
 
 helpCommand :: [String] -> String -> IO ()
 helpCommand args flags = do
+    putStrLn $ case args of
+        [] -> helpSummary
+        (command:[]) -> helpForCommand command
+        otherwise -> "Command takes at most 1 argument."
+
+
+helpSummary :: String
+helpSummary =
     let rows = map commandToRow commands
-    let keyWidth = maximum . map (length . fst) $ rows
-    let boxes = map (rowToBox keyWidth) rows
-    printBox $ vcat left boxes
+        keyWidth = maximum . map (length . fst) $ rows
+        boxes = map (rowToBox keyWidth) rows
+    in  render $ vcat left boxes
+
+
+helpForCommand :: String -> String
+helpForCommand command =
+    let commandObjectMaybe = find (\x -> name x == command) commands
+        docMaybe = fmap doc commandObjectMaybe
+    in  fromMaybe (command ++ " is not a command.") docMaybe
 
 
 commandToRow :: Command -> (String, String)
-commandToRow command = (name command, docDescription command)
+commandToRow command = (name command, docSummary command)
 
 
 rowToBox :: Int -> (String, String) -> Box
