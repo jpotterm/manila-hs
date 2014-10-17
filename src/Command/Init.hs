97980d4eff582fff90050d05c8a86e5428892448
Jonathan Potter <jpotterm@gmail.com>
2014-10-17 01:36:01 -0400

    Don't allow init in an occupied directory

--- src/Command/Init.hs
+++ src/Command/Init.hs
@@ -3,6 +3,7 @@ module Command.Init (initCommand) where
 import Data.Maybe
 import Database.HDBC
 import Database.HDBC.Sqlite3
+import System.Directory (doesFileExist)
 
 import Util
 import Command.Migrate
@@ -10,7 +11,12 @@ import Command.Migrate
 
 initCommand :: [String] -> String -> IO ()
 initCommand args flags = do
-    conn <- getDbConnection
-    migrate conn False
-    commit conn
-    disconnect conn
+    dbExists <- doesFileExist "manila.db"
+
+    if dbExists
+        then putStrLn "There is already a manila project in this directory."
+        else do
+            conn <- getDbConnection
+            migrate conn False
+            commit conn
+            disconnect conn
